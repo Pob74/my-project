fe3ad97264e986304ff516d0b840b9818b7c81a7
Zeljko Haberstok <zeljkohaberstok@gmail.com>
2021-01-29 13:16:19 +0100

    Firts commit

---
+++ src/components/Advice.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+    <h2>Here is your daily advice</h2>
+    <h3>{{advice}}</h3>
+    </div>
+</template>
+<script>
+export default {
+    created() {
+        fetch('https://api.adviceslip.com/advice')
+        .then((response) => response.json())
+        .then((result) => {
+            this.advice = result.slip.advice
+            console.log(result)
+        })
+        },
+    data(){
+        return {
+            advice: null
+        }
+    },
+}
+</script>
+<style scoped>
+h3 {
+        border:1px solid rgb(41, 37, 37);
+        background-color: gainsboro;
+        padding: 5vh 5vh 5vh 5vh;
+}
+</style>
+        
+       
+
+
+            
+
+    
+
---
+++ src/components/Message.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="message">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    msg:{
+      type: String,
+      required: true
+    }
+  },
+  data(){
+    return {
+      }
+  }
+}
+</script>
+  
+    
+
+  
+      
+
+    
+    
+  
+
+
